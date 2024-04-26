1820207ecef19aa1f149ded2b97728b8ef1bbb0c
Ahmed <example@gmail.com>
2024-04-26 22:47:24 +0300

    first

---
+++ js/lesson2.js
@@ -0,0 +1,34 @@
+const arr = [1, 'b', 3];
+
+arr[10] = '3456';
+
+console.log(arr[10]);
+
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    2: 'c',
+    abc: {
+        df: [{}, {}],
+        def: {
+
+        }
+    }
+}
+const b = 'b';
+
+arrObj[b] = '1234';
+
+console.log(arrObj.b);
+
+
+
+console.log(arr[1]);
+
+// const obj = {a: 1, b: 2};
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
+
+
